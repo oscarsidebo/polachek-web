67ba1893af08b89944d201fcf8af53e6aeac4a16
Oskar Sidebo <sideboo@gmail.com>
2022-05-12 21:46:40 -0700

    first draft

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: "#000",
+      white: "#fff",
+      primary: '#F47530',
+      secondary: '#FEE9DC',
+    },
+    fontFamily: {
+      serif: '"Innocence Latterve 2"',
+    },
+    spacing: {
+      8: '1.5rem',
+      16: '2rem',
+      24: '3rem',
+      32: '4rem',
+      40: '5rem',
+      48: '6rem',
+      56: '7rem',
+      64: '8rem',
+      72: '9rem',
+      80: '10rem',
+      88: '11rem',
+      96: '12rem',
+      104: '13rem',
+      112: '14rem',
+      120: '15rem',
+      128: '16rem',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
